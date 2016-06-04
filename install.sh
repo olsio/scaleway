588d8ea44d89c2a5b898259f4f67bec24d8c9e17
Oliver Schneider <os@ols.io>
2016-06-04 23:12:06 +0200

    ghost running

--- install.sh
+++ install.sh
@@ -1,30 +1,32 @@
 #!/bin/sh
 
 export DEBIAN_FRONTEND=noninteractive
-apt-get -q update
-apt-get -y -q upgrade
-apt-get install -y -q curl iptables iptables-persistent openvpn nginx supervisor zip uuid git mc wget vim
-apt-get clean
+apt-get -q update && \
+    apt-get -y -q upgrade && \
+    apt-get install -y -q curl iptables iptables-persistent openvpn nginx supervisor zip uuid git mc wget vim && \
+    apt-get clean
 
 rm -f /etc/nginx/sites-available/*
 rm -f /etc/nginx/sites-enabled/*
 
 rm -rf ./scaleway
 git clone https://github.com/olsio/scaleway.git scaleway
-cp -R ./scaleway/overlay/etc/ /etc/
-cp -R ./scaleway/overlay/usr/local/ /usr/local/
+cp -R ./scaleway/overlay/etc/* /etc
+cp -R ./scaleway/overlay/usr/local/* /usr/local
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
 source .bashrc
 
 nvm install v0.10.40
 
+n=$(which node);n=${n%/bin/node}; chmod -R 755 $n/bin/*; sudo cp -r $n/{bin,lib,share} /usr/local
+
 wget -qO ghost.zip https://ghost.org/zip/ghost-latest.zip && \
     rm -rf /var/www && \
     unzip ghost.zip -d /var/www/ && \
     rm -f ghost.zip && \
-    cd /var/www && npm install --production
-
+    cd /var/www && npm install --production && \
+    cd -
 
 useradd ghost && chown -R ghost:ghost /var/www
 
