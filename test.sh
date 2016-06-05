36f22f3c24d1e31655650db4d59e2f29f77f1311
Oliver Schneider <os@ols.io>
2016-06-05 10:49:54 +0200

    shell

--- test.sh
+++ test.sh
@@ -1,23 +1,23 @@
-#!/bin/bash
+#!/bin/sh
 
-while [[ -z "$DOMAIN" ]]
+while [ -z "$DOMAIN" ]
 do
   read -p "Enter the main domain for your blog: " DOMAIN
 done
 
-while [[ -z "$SSL_DOMAINS" ]]
+while [ -z "$SSL_DOMAINS" ]
 do
   read -p "Enter all domains that should be ssl protected: " SSL_DOMAINS
 done
 
-while [[ -z "$SMTP_USER" ]]
+while [ -z "$SMTP_USER" ]
 do
   read -p "Enter maingun SMTP user: " SMTP_USER
 done
 
-while [[ -z "$SMTP_USER" ]]
+while [ -z "$SMTP_PASSWORD" ]
 do
-  read -s -p "Enter maingun SMTP user: " SMTP_USER
+  read -s -p "Enter maingun SMTP password: " SMTP_PASSWORD
 done
 
-echo $DOMAIN $SSL_DOMAINS $SMTP_USER $SMTP_USER
+echo $DOMAIN $SSL_DOMAINS $SMTP_USER $SMTP_PASSWORD
